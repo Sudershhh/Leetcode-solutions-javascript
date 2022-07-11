506d7f7f09b8d23d3bd4955505e92eba19512a28
Sri Sudersan <tgsuder@gmail.com>
2022-07-11 23:01:41 +0530

    Contains Duplicate problem

---
+++ Arrays/ContainsDuplicate.js
@@ -0,0 +1,22 @@
+//LEETCODE PROBLEM 217
+
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+//TIME COMPLEXITY - 0(N)
+//SPACE COMPLEXITY - 0(N)
+
+
+var containsDuplicate = function(nums) {
+    let map = {}
+    for(let i =0;i<nums.length;i++)
+        {
+            if(!map[nums[i]])
+                {
+                    map[nums[i]]=true
+                }
+            else{
+                return true
+            }
+        }
+    return false;
+};
