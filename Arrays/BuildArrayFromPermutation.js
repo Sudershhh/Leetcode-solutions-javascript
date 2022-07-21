6d23f7a01f018ac0d8c34108057030f33c3df8db
Sri Sudersan <tgsuder@gmail.com>
2022-07-21 23:36:34 +0530

    Better Solution

--- Arrays/BuildArrayFromPermutation.js
+++ Arrays/BuildArrayFromPermutation.js
@@ -3,16 +3,35 @@
 //Build Array from Permutation - EASY
 
 
+
+
+
+//TIME COMPLEXITY - O(N+N) = O(2N) = O(N)
+//SPACE COMPLEXITY - O(1)
+var buildArray = function(nums) {
+
+for(let i=0;i<nums.length;i++)
+{
+    nums[i]= nums[i] + (nums[nums[i]]%1000)*1000
+}
+for(let i=0;i<nums.length;i++)
+{
+    nums[i]= Math.floor(nums[i]/1000)
+}
+return nums;
+}
+
+
 //TIME COMPLEXITY - O(N)
 //SPACE COMPLEXITY - O(N)
 
-var buildArray = function(nums) {
+// var buildArray = function(nums) {
     
     
-    let temp=[]
-    for(let i=0;i<nums.length;i++)
-        {
-            temp[i] = nums[nums[i]]
-        }
-    return temp
-};
+//     let temp=[]
+//     for(let i=0;i<nums.length;i++)
+//         {
+//             temp[i] = nums[nums[i]]
+//         }
+//     return temp
+// };
