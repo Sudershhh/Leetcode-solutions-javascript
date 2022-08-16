422f7b3a7fa0d636aac8f78b086bd9e7994d488a
Sri Sudersan <tgsuder@gmail.com>
2022-08-16 11:48:50 +0530

    Linked List cycle

---
+++ LinkedList/LinkedListCycle.js
@@ -0,0 +1,45 @@
+//LEETCODE PROBLEM 141
+
+//Linked List Cycle - EASY
+
+
+//Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+
+
+
+
+
+
+//TIME COMPLEXITY - O(N)
+//SPACE COMPLEXITY - O(N)
+var hasCycle = function(head) {
+    if(head == null || head.next == null)
+        {
+            return false;
+        }
+    
+    if(head == head.next.next)
+        {
+            return true
+        }
+    
+    let d1 = head
+    let set = new Set()
+    while(d1!=null)
+        {
+           if(set.has(d1.next))
+               {
+                   return true
+               }
+            
+            set.add(d1)
+            d1=d1.next
+        }
+    return false
+
+};
